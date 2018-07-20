ec82f773ea32d1640443495d8186251e507fd897
zhangqiaoli <zhangqiaoli1990@yeah.net>
2018-07-20 10:18:01 +0800

    v2.0 first version

--- Xlight_lib/debugDefine.h
+++ Xlight_lib/debugDefine.h
@@ -2,7 +2,7 @@
 #define _DEBUGDEFINE_H
 
 // Uncomment this line if in debug mode
-#define DEBUG_NO_WWDG
-#define DEBUG_LOG
+//#define DEBUG_NO_WWDG
+//#define DEBUG_LOG
 
 #endif /* _DEBUGDEFINE_H */
